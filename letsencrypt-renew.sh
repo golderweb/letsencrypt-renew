c0e5b48a1f6a6ce09cd32e103c7695cb388439ca
Jonathan Golder <jonathan@golderweb.de>
2017-02-09 21:11:58 +0100

    Add copyright information
    Add file header with copyright and licence information.
    Add GPLv3 licence full text.

--- letsencrypt-renew.sh
+++ letsencrypt-renew.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+#
+#  letsencrypt-renew.sh
+#
+#  Copyright 2017 Jonathan Golder <jonathan@golderweb.de>
+#
+#  Derived from:
+#    https://wiki.uberspace.de/webserver:https#automatisieren_von_let_s_encrypt
+#  Which is itself originaly based on:
+#    https://github.com/nerdoc/uberspace-tools/blob/master/letsencrypt-renew
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 
 # PATH to inifiles / letsencrypt dir
